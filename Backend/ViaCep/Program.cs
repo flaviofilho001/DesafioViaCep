1a14faad63b31221fe8104edcf86b1e96a781483
flaviofilho001 <flaviomesquitamarinho13@gmail.com>
2026-08-01 15:30:32 -0300

    feat(swagger): instala o Swashbuckle e configura a documentacao interativa da API no Swagger UI

--- Backend/ViaCep/Program.cs
+++ Backend/ViaCep/Program.cs
@@ -7,7 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -21,6 +22,11 @@ app.UseMiddleware<ExceptionHandlingMiddleware>();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "ViaCep API v1");
+    });
     app.MapOpenApi();
 }
 
