fefcda4f885c3a8acbd2bd3d4a029dd8206037e9
flaviofilho001 <flaviomesquitamarinho13@gmail.com>
2026-08-01 12:56:21 -0300

    feat: cria entidades de dominio Usuario e Endereco para o backend

---
+++ Backend/ViaCep/Models/Endereco.cs
@@ -0,0 +1,45 @@
+﻿namespace ViaCep.Models
+{
+    public class Endereco
+    {
+        public string Cep { get; set; }
+        public string Logradouro { get; set; }
+        public string Complemento { get; set; }
+        public string Unidade { get; set; }
+        public string Bairro { get; set; }
+        public string Localidade { get; set; }
+        public string Uf { get; set; }
+        public string Estado { get; set; }
+        public string Regiao { get; set; }
+        public string Ibge { get; set; }
+        public string Gia { get; set; }
+        public string Ddd { get; set; }
+        public string Siafi { get; set; }
+
+    }
+}
+
+
+/*
+
+Retorno da API ViaCep para o CEP 01001-000:
+
+URL: viacep.com.br/ws/01001000/json/
+
+    {
+      "cep": "01001-000",
+      "logradouro": "Praça da Sé",
+      "complemento": "lado ímpar",
+      "unidade": "",
+      "bairro": "Sé",
+      "localidade": "São Paulo",
+      "uf": "SP",
+      "estado": "São Paulo",
+      "regiao": "Sudeste",
+      "ibge": "3550308",
+      "gia": "1004",
+      "ddd": "11",
+      "siafi": "7107"
+    }
+         
+*/
---
+++ Backend/ViaCep/Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using ViaCep.Models;
+
+namespace ViaCep.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string NomeUsuario { get; set; } 
+        public string SenhaHash { get; set; } 
+        public ICollection<Endereco> Enderecos { get; set; } = new List<Endereco>();
+    }
+}
+
